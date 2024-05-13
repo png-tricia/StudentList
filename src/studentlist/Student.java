6162482fc975b11df6f12821dde3c59851fdaa13
garia <garia@10.48.18.12>
2024-05-13 13:58:53 -0400

    initial commit

---
+++ src/studentlist/Student.java
@@ -0,0 +1,20 @@
+/**
+ * Class: Software Design and Development
+ * Date: Monday 6, May 2024
+ * Exercise 1: create a Student class
+ */
+package studentlist;
+
+public class Student {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
---
+++ src/studentlist/StudentList.java
@@ -0,0 +1,32 @@
+/**
+ * Class: Software Design and Development
+ * Date: Monday 6, May 2024
+ * Exercise 1: create a Student List
+ */
+package studentlist;
+
+public class StudentList {
+
+    public static void main(String[] args) {
+        // Create an array to store n students
+        Student[] studentList = new Student[3];
+        Student s1 = new Student();
+        s1.setName("Patricia Gariando");
+        Student s2 = new Student();
+        s2.setName("Peppa Pig");
+        Student s3 = new Student();
+        s3.setName("Elmo's World");
+
+        // Store student names inside Student[]
+        studentList[0] = s1;
+        studentList[1] = s2;
+        studentList[2] = s3;
+
+        // Display all Student names
+        for (Student s : studentList) {
+            System.out.println(s.getName());
+        }
+
+    }
+
+}
